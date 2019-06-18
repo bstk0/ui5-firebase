d3404c37543f094b41c234e4b699fe67971cd821
bstk <rodrigo.bisterco@gmail.com>
2019-06-18 15:01:17 +0000

    realtime

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -24,7 +24,10 @@ sap.ui.define([
 			this.getView().setModel(shipmentModel);
 
 			// Get single set of shipments once
-			this.getShipments(collRefShipments);
+			//this.getShipments(collRefShipments);
+
+			// Get realtime shipments
+			this.getRealTimeShipments(collRefShipments);
 		},
 		getShipments: function (collRefShipments) {
 			collRefShipments.get().then(
@@ -37,6 +40,50 @@ sap.ui.define([
 					shipmentData.shipments = shipments;
 					this.getView().byId("shipmentTable").getBinding("items").refresh();
 				}.bind(this));
+		},
+		getRealTimeShipments: function (collRefShipments) {
+			// The onSnapshot the keep the data up to date in case of added, 
+			// modified or removed data in the Firestor database
+			collRefShipments.onSnapshot(function (snapshot) {
+				// Get the shipment model
+				var shipmentModel = this.getView().getModel();
+				// Get all the shipments
+				var shipmentData = shipmentModel.getData();
+
+				// Get the current added/modified/removed document (shipment) 
+				// of the collection (shipments)
+				snapshot.docChanges().forEach(function (change) {
+					// set id (to know which document is modifed and
+					// replace it on change.Type == modified) 
+					// and data of firebase document
+					var oShipment = change.doc.data();
+					oShipment.id = change.doc.id;
+
+					// Added document (shipment) add to arrat
+					if (change.type === "added") {
+						shipmentData.shipments.push(oShipment);
+					}
+					// Modified document (find its index and change current doc 
+					// with the updated version)
+					else if (change.type === "modified") {
+						var index = shipmentData.shipments.map(function (shipment) {
+							return shipment.id;
+						}).indexOf(oShipment.id);
+						shipmentData.shipments[index] = oShipment;
+					}
+					// Removed document (find index and remove it from the shipments array)
+					else if (change.type === "removed") {
+						var index = shipmentData.shipments.map(function (shipment) {
+							return shipment.id;
+						}).indexOf(oShipment.id);
+						shipmentData.shipments.splice(index, 1);
+					}
+				});
+
+				//Refresh your model and the binding of the items in the table
+				this.getView().getModel().refresh(true);
+				this.getView().byId("shipmentTable").getBinding("items").refresh();
+			}.bind(this));
 		}
 	});
 });
